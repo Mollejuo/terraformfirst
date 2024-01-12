563f88a6be86af9612f297a3294c4eebfbbacaf7
Mollejuo <2822097+Mollejuo@users.noreply.github.com>
2024-01-12 17:51:32 -0500

    Fixing errors in code

--- main.tf
+++ main.tf
@@ -55,17 +55,19 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
+  target_groups = [
+    {
+      name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       targets = {
+        my_target = {
           target_id = "aws_instance.blog.id"
+          port = 80
       }
     }
-  }
+  ]
 
 http_tcp_listeners = [
   {
