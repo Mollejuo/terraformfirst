11ca1c6cde9db8c9b4e53a716232cb198da35171
Mollejuo <2822097+Mollejuo@users.noreply.github.com>
2024-01-12 18:00:47 -0500

    Fixing Errors #4

--- main.tf
+++ main.tf
@@ -51,6 +51,9 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
+
+load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -58,17 +61,17 @@ module "alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = "aws_instance.blog.id"
+          target_id = aws_instance.blog.id
           port = 80
-      }
+        }
+      } 
     }
-    }
-    ]
+  ]
 
 http_tcp_listeners = [
   {
